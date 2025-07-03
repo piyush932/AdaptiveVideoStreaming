41c2c4a941eef6a944cb3aa13c813074441952ee
Piyush <b521044@iiit-bh.ac.in>
2025-07-03 17:35:52 +0530

    Add routing layer

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,11 @@
-import express,{Express,Request,Response} from 'express';
+import express,{Express} from 'express';
+import { PORT } from './config/server.config';
+import apiRouter from './routes';
 
 const app:Express = express();
 
-app.get('/ping',(_req:Request,res:Response)=>{
-    res.json({
-        message:'pong!!!'
-    })
-})
+app.use('/api',apiRouter);
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
 })
